cf67d2bada4af4cc59a367403d9656f8c273a477
Fabricio Nogueira <nogsantos@gmail.com>
2016-05-10 07:15:30 -0300

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+/**
+ * Módulos GRUNT ambiente de desenvolvimento
+ */
+(function(){
+    "use strict";
+    module.exports = function (grunt) {
+        var gruntConfig = {
+            less: {
+                development: {
+                    options: {
+                        compress: true
+                    },
+                    files: {
+                        'css/main.min.css': ["desenvolvimento/css/main.less"]
+                    }
+                }
+            },
+            uglify: {
+                options: {
+                    mangle: true,
+                    report: 'gzip'
+                },
+                my_target: {
+                    files: {
+                        'js/main.min.js': [
+                            'desenvolvimento/js/*/*.js',
+                            'desenvolvimento/js/*/*/*.js',
+                            'desenvolvimento/js/controller.js',
+                        ]
+                    }
+                }
+            },
+            watch : {
+                dist : {
+                    files : [
+                        'desenvolvimento/js/**/*',
+                        'desenvolvimento/css/**/*'
+                    ],
+                    tasks : [ 'uglify', 'less' ]
+                }
+            }
+        };
+
+        grunt.loadNpmTasks('grunt-contrib-less');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+
+        grunt.initConfig(gruntConfig);
+
+        var keys  = Object.keys(gruntConfig);
+        var tasks = [];
+        var i = 1;
+
+        for(i in keys){
+            tasks.push(keys[i]);
+        }
+
+        grunt.registerTask('default', tasks);
+        grunt.registerTask( 'w',['watch']);
+    };
+
+}());
---
+++ README.md
@@ -0,0 +1,7 @@
+# Exercício avaliativo A1
+
+Regras:
+1. Crie uma lista de Pessoas e armazene seus dados em um localStorage.
+2. Busque as informações contidas na storage e apresente ao usuário
+3. Você deve criar um formulário e listar as informações para o usuário
+4. Utilize HTML5. Sem bootstrap.
